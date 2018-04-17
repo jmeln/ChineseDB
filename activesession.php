bde7af9f6e843d219aec6b33db5d0450167f550f
Jarrett Melnick <melnick.jarrett@hotmail.com>
2018-04-17 17:26:00 -0400

    Added login capabilities and search function

--- activesession.php
+++ activesession.php
@@ -8,9 +8,7 @@
   <link rel="stylesheet" href="assets/css/main.css" />
   <link rel="icon" href="assets/images/favicon.ico" sizes="32x32" />
 
-  <!-- JAVASCRIPT HERE -->
-  <script type="text/javascript" src="assets/scripts/sort.js"></script>
-  <script type="text/javascript" src="assets/scripts/displayDropdown.js"></script>
+  <script type="text/javascript" src="scripts/sorttable.js"></script>
 </head>
 
 <div id="container">
@@ -18,37 +16,13 @@
   <div id="header">
     <div id="loginContainer">
       <!-- PHP CODE TO PULL THE CURRENT USER SESSION INFORMATION FROM USER TABLE -->
-      <p>$USERNAME $USERSESSION</p>
+      <p><?php echo "Welcome, " . "Admin" . "!"?></p>
     </div>
     <h1>Mercer HSK Database</h1>
   </div>
 
   <div id="body">
-    <div class="dropdown">
-      <button onclick="toggleDropdown()" class="dropbtn">Element</button>
-        <div id="dropdownMenu" class="dropdown-content">
-          <button>Character</button>
-          <button>Pinyin</button>
-          <button>Definition</button>
-          <button>Radical</button>
-          <button>Stroke Count</button>
-          <button>HSK Level</button>
-          <button>Frequency Rank</button>
-        </div>
-      <button onclick="toggleDropdown()" class="dropbtn">Operator</button>
-        <div id="dropdownMenu" class="dropdown-content">
-          <button>></button>
-          <button>=</button>
-          <button><</button>
-          <button>!=</button>
-        </div>
-      <button onclick="toggleDropdown()" class="dropbtn">Condition</button>
-        <div id="dropdownMenu" class="dropdown-content">
-          <button>Placeholder</button>
-        </div>
-    </div>
-    
-    <table id="data">
+    <table id="data" class="sortable">
       <tr>
         <th onclick="sortTable(0)">Character</th>
         <th onclick="sortTable(1)">Pinyin</th>
@@ -83,7 +57,7 @@
       //Prints the result of the query. [add HTML tags here START]
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-          echo "<tr><td>".$row["CHNcharacter"]."</td><td>".$row["Pinyin"]."</td><td>".$row["Definition"]."</td><td>".$row["Radical"]."</td><td>".$row["StrokeCount"]."</td><td>".$row["HSKlevel"]."</td><td>";
+          echo "<tr><td>".$row["CHNcharacter"]."</td><td>".$row["Pinyin"]."</td><td>".$row["Definition"]."</td><td>".$row["Radical"]."</td><td>".$row["StrokeCount"]."</td><td>".$row["HSKlevel"]."</td><td>".$row["FrequencyRank"];
         }
         echo "</table>";
       //[add HTML tags here END]
@@ -100,7 +74,7 @@
   </div>
 
   <div id="footer">
-    <p>Devleoped by Cody Lee, Jarrett Melnick</p>
+    <p>Devleoped by Cody Lee, Jarrett Melnick, <mark>and Vincent Raymond</mark>  </p>
     <a href="https://opensource.org/licenses/MIT">© Copyright 2018 Jarrett Melnick & Cody Lee. MIT</a>
   </div>
 </div>
--- php/login.php
+++ php/login.php
@@ -1,28 +1,50 @@
 <?php
-      header('Content-Type: text/html; charset=utf-8');
-      $username= "root";
-      $password = "";
-      $mysqli = new mysqli("localhost", $username, $password, "group6");
-      if ($mysqli->connect_error) {
-        die("Connection failed: " . $mysqli->connect_error);
-      }
-      mysqli_set_charset($mysqli,"utf8");
-      $result = $mysqli->query("SELECT * FROM user");
-      if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-			if($row["Username"] == $_POST["name"]){
-				if($row["Password"] == $_POST["psw"]){
-					header("Location: ../index.php");
-					exit;
-				}else{
-					echo "Fail! Wrong password";
-				}
-			}else{
-				echo "USER NOT FOUND!";
-			}
-		}
-      }else{
-        echo "0 results";
-	  }
-      $mysqli->close();
+//Program to process login requests to the Webite
+
+//Establishes connection to the database.
+header('Content-Type: text/html; charset=utf-8');
+$username= "root";
+$password = "";
+$mysqli = new mysqli("localhost", $username, $password, "group6");
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
+mysqli_set_charset($mysqli,"utf8");
+
+//Queries the database to gather users
+$result = $mysqli->query("SELECT * FROM user");
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()){
+    if($row["Username"] == $_POST["name"]){
+      //Checks if the username is found.
+      if($row["Password"] == $_POST["psw"]){
+        //If the password provided matches the database password, go to new webpage and exit. 
+        #TODO: Change to webpage clone.SDS
+       header("Location: ../activesession.php");
+       exit;
+     }else{
+        //Reloads the page if the passwod is wrong
+      header("Location: ../index.php");
+	  //header("Location: ../assets/ErrorPages/error.html");
+      exit;
+     }
+    }else{
+      //Reloads the page if user is not found. 
+      #TODO: Create a new user page if user is not found.
+      header("Location: ../index.php");
+	  //header("Location: ../assets/ErrorPages/error.html");
+      exit; 
+		//echo $row["Username"]."=".$_POST["name"];
+	//echo $row["Password"]."=".$_POST["psw"];
+    } 
+  }
+}else{
+  //Prints if no users exist.
+  //If this prints, make sure the server is not on fire. 
+  echo "ERROR 505! SOMETHING WENT HORRIBLY WRONG!";
+  header("Location: ../assets/ErrorPages/error.html");
+  exit;
+}
+$mysqli->close();
+
 ?>
